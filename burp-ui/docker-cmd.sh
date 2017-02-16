b39f421d68b510815242617e97f3295c54483400
Peter Schiffer <pschiffe@redhat.com>
2017-02-16 18:29:08 +0100

    Specify LANG env var

--- burp-ui/docker-cmd.sh
+++ burp-ui/docker-cmd.sh
@@ -10,5 +10,6 @@ fi
 
 envtpl < /etc/burp/burpui.cfg.tpl > /etc/burp/burpui.cfg
 
+export LANG='en_US.UTF-8'
 exec /usr/bin/gunicorn -k gevent -w 4 --access-logfile - -b '0.0.0.0:5000' 'burpui:create_app(conf="/etc/burp/burpui.cfg", verbose=3)'
 #exec /usr/bin/python3-gunicorn -k gevent -w 4 --access-logfile - -b '0.0.0.0:5000' 'burpui:create_app(conf="/etc/burp/burpui.cfg", verbose=3)'
